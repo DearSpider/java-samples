2695c95682640490abb98edc7ce385d951e0a11a
DearSpider <55309900+DearSpider@users.noreply.github.com>
2019-09-28 21:40:10 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 public class test1 { 
 	public static void main(String[] args) 
 	{ 
 		System.out.println("I am starting to use Github"); 
+		System.out.print("What is your name "); 
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String k = scan.next();
+		
+		System.out.print("Hi "+k+", I'm starting new git repos"); 
 	} 
 } 
